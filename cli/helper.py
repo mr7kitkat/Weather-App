cc9d72481406208adc3250daf41a3eddfb30e1fc
Aman <mr7kitkat@gmail.com>
2022-12-09 01:04:41 +0530

    format date, time and wind speed for easy understanding

--- cli/helper.py
+++ cli/helper.py
@@ -3,12 +3,25 @@ from pyfiglet import Figlet
 from tabulate import tabulate
 from expections import *
 from sys import exit, argv
+from datetime import datetime, date
 
 
 # turning kelvin to celcius
 def kelvinToCelcius(kelvin):
     return f'{round(kelvin - 273.15)}°C'
 
+# Get date
+def getDate(timestamp):
+    dt = date.fromtimestamp(timestamp)
+    return dt.strftime("%d %b")
+
+# get time
+def getTime(timestamp):
+    dt = datetime.fromtimestamp(timestamp)
+    return dt.strftime("%X")
+    
+def formatWindSpeed(floatNum):
+    return f"{round(floatNum * 3.6)} km/h"
 
 # Making network requests
 def make_request(location, apikey):
@@ -35,11 +48,11 @@ def cleanData(rawdata):
         "Sea Level": rawdata["main"].get("sea_level", 0),
         "Ground Level": rawdata["main"].get("grnd_level", 0),
         "Visibility": f'{round(rawdata["visibility"] / 100)} %',
-        "Wind Speed": rawdata["wind"].get("speed", 0),
+        "Wind Speed": formatWindSpeed(rawdata["wind"].get("speed", 0)),
         "Clouds": f'{rawdata["clouds"]["all"]} %',
-        "Date": rawdata["dt"],  
-        "Sunrise": rawdata["sys"]["sunrise"],
-        "Sunset": rawdata["sys"]["sunset"],
+        "Date": getDate(rawdata["dt"]),  
+        "Sunrise": getTime(rawdata["sys"]["sunrise"]),
+        "Sunset": getTime(rawdata["sys"]["sunset"]),
         "City": rawdata["name"],
         "Country": rawdata["sys"]["country"]
     }
